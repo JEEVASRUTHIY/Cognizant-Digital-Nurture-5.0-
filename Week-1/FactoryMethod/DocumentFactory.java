4659930869a9c0b600724dd335fae9891e647556
JEEVASRUTHI Y <230125.cs@rmkec.ac.in>
2026-07-08 11:54:25 +0530

    factory method

---
+++ Week-1/FactoryMethod/DocumentFactory.java
@@ -0,0 +1,47 @@
+// Document Interface
+interface Document {
+    void open();
+}
+
+// Concrete Document Classes
+class WordDocument implements Document {
+    public void open() {
+        System.out.println("Opening Word Document...");
+    }
+}
+
+class PdfDocument implements Document {
+    public void open() {
+        System.out.println("Opening PDF Document...");
+    }
+}
+
+class ExcelDocument implements Document {
+    public void open() {
+        System.out.println("Opening Excel Document...");
+    }
+}
+
+// Abstract Factory
+abstract class DocumentFactory {
+    public abstract Document createDocument();
+}
+
+// Concrete Factories
+class WordFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new WordDocument();
+    }
+}
+
+class PdfFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new PdfDocument();
+    }
+}
+
+class ExcelFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new ExcelDocument();
+    }
+}
