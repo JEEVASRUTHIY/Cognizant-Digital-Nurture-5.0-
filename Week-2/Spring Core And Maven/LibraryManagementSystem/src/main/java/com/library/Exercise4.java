30dfe71cde3ab9c5b839825a0d093ae2559a3af8
JEEVASRUTHI Y <230125.cs@rmkec.ac.in>
2026-07-09 11:24:32 +0530

    project

---
+++ Week-2/Spring Core And Maven/LibraryManagementSystem/src/main/java/com/library/Exercise4.java
@@ -0,0 +1,23 @@
+package com.library;
+
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Exercise4 {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService service =
+                (BookService) context.getBean("bookService");
+
+        service.display();
+
+        System.out.println("Maven Project Configured Successfully");
+
+    }
+
+}
