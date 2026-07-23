fe111fd27be5d13f6e2ebce4b5754b4bb458fbd6
JEEVASRUTHI Y <230125.cs@rmkec.ac.in>
2026-07-23 19:19:58 +0530

    microservices

---
+++ Microservices/account/src/main/java/com/cognizant/account/controller/AccountController.java
@@ -0,0 +1,41 @@
+package com.cognizant.account.controller;
+
+import com.cognizant.account.model.Account;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(
+                    AccountController.class
+            );
+
+    @GetMapping("/accounts/{number}")
+    public Account getAccount(
+            @PathVariable String number) {
+
+        LOGGER.info(
+                "Account request received for number: {}",
+                number
+        );
+
+        Account account =
+                new Account(
+                        number,
+                        "savings",
+                        234343
+                );
+
+        LOGGER.info(
+                "Returning account: {}",
+                account
+        );
+
+        return account;
+    }
+}
